1062a05bc459a15228f6e34a90fdf35807ddffaf
ranipriya-hub <ranipriya8.rp8@gmail.com>
2023-04-07 20:22:14 +0530

    datatypes decalaration

--- src/test/Number.java
+++ src/test/Number.java
@@ -4,14 +4,20 @@ public class Number {
 
 	public static void main(String[] args) {
 		int a=10;
+		int b=5;
 	float x=110.5f;
 	char ch ='a';
 	double num=120.567;
-	
+	long l=12345678945l;
+	boolean r=true;
+	System.out.println(a+b);
+	System.out.println("the sumof number is:" +a+b);
 		System.out.println(a);
 		System.out.println(x);
 		System.out.println(ch);
 		System.out.println(num);
+		System.out.println(l);
+		System.out.println(r);
 
 	}
 
