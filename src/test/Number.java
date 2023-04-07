6e1ec2e9c627d94f809be4c2b26ddbdaa122d92e
ranipriya-hub <ranipriya8.rp8@gmail.com>
2023-04-07 11:57:05 +0530

    datatypes variable declaration

--- src/test/Number.java
+++ src/test/Number.java
@@ -4,7 +4,14 @@ public class Number {
 
 	public static void main(String[] args) {
 		int a=10;
+	float x=110.5f;
+	char ch ='a';
+	double num=120.567;
+	
 		System.out.println(a);
+		System.out.println(x);
+		System.out.println(ch);
+		System.out.println(num);
 
 	}
 
