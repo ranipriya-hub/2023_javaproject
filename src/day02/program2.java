57366bac7cf777ed13cd782d9c294bfd6c1db881
ranipriya-hub <ranipriya8.rp8@gmail.com>
2023-04-10 20:14:21 +0530

    storing member with nonstatic void use to create object to add stsatic member,to accessthe function use dot  functions

---
+++ src/day02/program2.java
@@ -0,0 +1,15 @@
+package day02;
+
+public class program2 {
+
+	public static void main(String[] args) {
+		addfunction(10,20);
+		
+	}
+	public static void addfunction(int a, int b) {
+		//Addfunction(a,b);
+		System.out.println(a+b);
+		
+	}
+
+}
--- src/test/Number.java
+++ src/test/Number.java
@@ -4,14 +4,13 @@ public class Number {
 
 	public static void main(String[] args) {
 		int a=10;
-		int b=5;
 	float x=110.5f;
 	char ch ='a';
+	short a2=32767;
 	double num=120.567;
 	long l=12345678945l;
 	boolean r=true;
-	System.out.println(a+b);
-	System.out.println("the sumof number is:" +a+b);
+	System.out.println(a2);
 		System.out.println(a);
 		System.out.println(x);
 		System.out.println(ch);
